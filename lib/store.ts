f1b13acb1711a6637ec155f1b5ac687aa4cd099a
manisense <imanishmeenaa@gmail.com>
2025-12-12 21:08:10 +0530

    Setup Redux, React Query, and install shadcn components

---
+++ lib/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
+
+// We will add slices here later
+export const store = configureStore({
+  reducer: {
+    // trade: tradeReducer
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
